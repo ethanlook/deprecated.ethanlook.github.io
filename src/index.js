110f6d17a70aa2b9fe85cc59aa38ce6d4b689cda
--add <ethan@lookfamily.org>
2016-08-28 19:04:32 -0500

    first build

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var ReactDOM = require('react-dom');
+var React = require('react');
+ 
+var Resume = require('./resume');
+ 
+ReactDOM.render(
+  <Resume />,
+  document.getElementById('app')
+);
---
+++ src/resume.js
@@ -0,0 +1,9 @@
+var React = require('react');
+ 
+var Resume = React.createClass({
+  render: function() {
+    return <h1>Hello, welcome to my website!</h1>;
+  }
+});
+
+module.exports = Resume;
